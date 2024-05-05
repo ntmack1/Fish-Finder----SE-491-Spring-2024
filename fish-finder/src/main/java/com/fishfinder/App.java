52e9696a0b7dc4baf441c936ba03d8e78de684e9
Wasif Siddiqui <wasif20@outlook.com>
2024-05-04 19:34:16 -0500

    [WS] Adding Initial File Structure as a maven project

---
+++ fish-finder/src/main/java/com/fishfinder/App.java
@@ -0,0 +1,13 @@
+package com.fishfinder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
