97b1078d551c503fcfce7f358aedc18f653dd539
brandon1202h <brandon1202h@gmail.com>
2024-05-06 00:22:55 -0400

    Created Busobj For MealDB & NAS

---
+++ fish-finder/src/main/java/com/fishfinder/busobj/FishBusobj.java
@@ -0,0 +1,20 @@
+package com.fishfinder.busobj;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FishBusobj {
+    private Integer speciesId;
+    private String group;
+    private String family;
+    private String genus;
+    private String species;
+    private String subspecies;
+    private String variety;
+    private String authority;
+    private String commonName;
+    private String nativeExotic;
+    private String FMB;
+}
