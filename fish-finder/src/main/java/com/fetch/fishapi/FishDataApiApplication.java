9fe8d27ed7db2a28c15bbb2f178617b188a9db17
nikhilmeda <nikhilmeda@depaul.edu>
2024-05-14 18:27:28 -0400

    Code to fetch the Fish Data from Json file
    Java Files:
    Fish.java
    FishController.java
    FishDataApiApplication.java
    fish_data.json has the fish details
    application.properties has the details of local host and other

---
+++ fish-finder/src/main/java/com/fetch/fishapi/FishDataApiApplication.java
@@ -0,0 +1,11 @@
+package com.fetch.fishapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishDataApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FishDataApiApplication.class, args);
+    }
+}
