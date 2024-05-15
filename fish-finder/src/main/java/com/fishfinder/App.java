0ab6f4d604213c395fa7b4f1b3ec374e3837ca9e
ntmack1 <ntmack5@gmail.com>
2024-05-14 23:46:43 -0500

    Basic frontend search feature using Nikhil's backend logic

--- fish-finder/src/main/java/com/fishfinder/App.java
+++ fish-finder/src/main/java/com/fishfinder/App.java
@@ -1,16 +1,16 @@
-package com.fishfinder;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-/**
- * Hello world!
- *
- */
-@SpringBootApplication
-public class App 
-{
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-}
+//package com.fishfinder;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+///**
+// * Hello world!
+// *
+// */
+//@SpringBootApplication
+//public class App
+//{
+//    public static void main(String[] args) {
+//        SpringApplication.run(App.class, args);
+//    }
+//}
