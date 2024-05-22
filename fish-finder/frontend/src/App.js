6c48ae9321c251ab5ae33134c2dac5a9f96f520e
ntmack1 <ntmack5@gmail.com>
2024-05-22 01:26:38 -0500

    Fish.js was deleted, as it was redundant to app.js. I also relocated FishSearch.js to components/search to make it more organized.

--- fish-finder/frontend/src/App.js
+++ fish-finder/frontend/src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Paths } from './components/Paths';
-import Search from './FishSearch'; // This is your Search component
+import FishSearch from './components/Search/FishSearch'; // This is your Search component
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    <Route path="/" element={<Search />} />
+                    <Route path="/" element={<FishSearch />} />
                     <Route path="/*" element={<Paths />} />
                 </Routes>
             </div>
