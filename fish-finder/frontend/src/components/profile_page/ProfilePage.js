8ec3cd26d0f73cbd511a2bf84ee759eb5fb08830
ntmack1 <ntmack5@gmail.com>
2024-06-10 08:33:26 -0500

    Profile page displays the logged in user's name

--- fish-finder/frontend/src/components/profile_page/ProfilePage.js
+++ fish-finder/frontend/src/components/profile_page/ProfilePage.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import Header from "../header/Header";
+import { jwtDecode } from 'jwt-decode';
+import AuthContext from '../../context/AuthProvider'
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+
 
 class ProfilePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: 'John Doe',
-            email: 'john.doe@example.com',
-            bio: 'Software Developer at XYZ Company'
+            name: '' // Initialize name as empty
         };
+    }
+
+    // Use componentDidMount to decode the token
+    componentDidMount() {
+        this.context.auth.accessToken && this.decodeToken(this.context.auth.accessToken);
+    }
+
+    decodeToken = (token) => {
+        if (token) {
+            const decoded = jwtDecode(token);
+            this.setState({ name: decoded.sub });
+        }
+    }
+
+    handleLogout = () => {
+        this.context.logout();
+        window.location.reload();
     }
 
     render() {
         return (
             <div>
                 <Header/>
-                <h1>{this.state.name}</h1>
-                <p>Email: {this.state.email}</p>
-                <p>Bio: {this.state.bio}</p>
+                {this.context.auth.accessToken ? (
+                    <>
+                        <h1>{this.state.name}</h1>
+                        <button onClick={this.handleLogout}>Logout</button>
+                    </>
+                ) : (
+                    <h1><Link to="/login">Please log in</Link></h1> // Use Link to create a hyperlink
+                )}
             </div>
         );
     }
 }
+
+ProfilePage.contextType = AuthContext; // Assign AuthContext to contextType
 
 export default ProfilePage;
