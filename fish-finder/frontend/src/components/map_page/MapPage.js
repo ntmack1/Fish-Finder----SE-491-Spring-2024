b449c86afce765e0aba212264bcb814106d763c4
Wasif Siddiqui <wasif20@outlook.com>
2024-06-10 23:08:24 -0500

    [WS] Added or tried to add google analytics metric and exposed metrics for prometheus

--- fish-finder/frontend/src/components/map_page/MapPage.js
+++ fish-finder/frontend/src/components/map_page/MapPage.js
@@ -2,16 +2,30 @@
 import React from 'react';
 import InteractiveMap from './InteractiveMap';
 import Header from "../header/Header";
+import ReactGA from 'react-ga4';
 
 const MapPage = () => {
     const handleSave = (lat, lng) => {
         console.log('Saved location:', lat, lng);
-        // Handle the saved coordinates, e.g., save them to a database or state.
+        // Send coordinates to Google Analytics as an event
+        try {
+            ReactGA.event({
+                name: 'saved_location',
+                params: {
+                    latitude: lat,
+                    longitude: lng,
+                    coordinates: `(${lat}, ${lng})`
+                }
+            });
+            console.log('GA event sent: ', { name: 'saved_location', params: { latitude: lat, longitude: lng, coordinates: `(${lat}, ${lng})` } });
+        } catch (error) {
+            console.error('Error sending GA event: ', error);
+        }
     };
 
     return (
         <div>
-            <Header/>
+            <Header />
             <h1>Map Page</h1>
             <InteractiveMap onSave={handleSave} />
         </div>
